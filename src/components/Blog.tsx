534959e4316d1a7c4d1703b635dfbbba1e40ad7e
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-07-05 12:12:23 +0530

    added blog

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Box,
   Heading,
@@ -8,115 +8,96 @@ import {
   Divider,
   HStack,
   Tag,
-  Wrap,
-  WrapItem,
   SpaceProps,
-  useColorModeValue,
   Container,
-  VStack,
-  Flex,
-} from "@chakra-ui/react";
-import { BlogPageEdgeType, BlogPageType } from "../pages";
+  SimpleGrid,
+} from '@chakra-ui/react';
+import { BlogPageType } from '../pages';
 
 interface IBlogTags {
   tags: Array<string>;
-  marginTop?: SpaceProps["marginTop"];
+  marginTop?: SpaceProps['marginTop'];
 }
 
 const BlogTags: React.FC<IBlogTags> = (props) => {
   return (
     <HStack spacing={2} marginTop={props.marginTop}>
-      {props.tags.map((tag) => {
-        return (
-          <Tag size={"md"} variant="solid" colorScheme="orange" key={tag}>
-            {tag}
-          </Tag>
-        );
-      })}
+      {props.tags.map((tag) => (
+        <Tag size={'md'} variant='solid' colorScheme='red' key={tag}>
+          {tag}
+        </Tag>
+      ))}
     </HStack>
   );
 };
 
 interface BlogAuthorProps {
-  date: string;
   name: string;
 }
 
-type blogType = NonNullable<BlogPageType>;
-
-export const BlogAuthor: React.FC<BlogAuthorProps> = (props) => {
+const BlogAuthor: React.FC<BlogAuthorProps> = ({ name }) => {
   return (
-    <HStack marginTop="2" spacing="2" display="flex" alignItems="center">
+    <HStack marginTop='2' spacing='2' display='flex' alignItems='center'>
       <Image
-        borderRadius="full"
-        boxSize="40px"
-        src="https://100k-faces.glitch.me/random-image"
-        alt={`Avatar of ${props.name}`}
+        borderRadius='full'
+        boxSize='40px'
+        src='https://100k-faces.glitch.me/random-image'
+        alt={`Avatar of ${name}`}
       />
-      <Text fontWeight="medium">{props.name}</Text>
-      <Text>—</Text>
-      <Text>{props.date}</Text>
+      <Text fontWeight='medium'>{name}</Text>
     </HStack>
   );
 };
 
-const FeaturedBlogs = (blogs: blogType) => {
-  console.log(typeof blogs)
+const FeaturedBlogs: React.FC<BlogPageType> = ({ edges }) => {
+  const blogList = edges.slice(0, 3); 
   return (
-    <Container maxW={"6xl"} as="section" p="12">
-      <Heading as="h2" marginTop="5" textAlign={"center"}>
+    <Container maxW={'6xl'} as='section' p='12'>
+      <Heading as='h2' marginTop='5' textAlign={'center'}>
         Latest articles
       </Heading>
-      <Divider marginTop="5" />
-      <Flex
-        marginTop="5"
-        flexWrap={"wrap"}
-        justifyContent={"space-between"}
-        rowGap={"100"}
-      >
-        {blogs.edges.map((item) => {
-          return (
-            <WrapItem width={{ base: "100%", sm: "45%", md: "30%" }}>
-              <Box w="100%">
-                <Box borderRadius="lg" overflow="hidden">
-                  <Link
-                    textDecoration="none"
-                    _hover={{ textDecoration: "none" }}
-                  >
-                    <Image
-                      transform="scale(1.0)"
-                      src={item.blog.frontmatter?.image!}
-                      alt="some text"
-                      objectFit="contain"
-                      width="100%"
-                      transition="0.3s ease-in-out"
-                      _hover={{
-                        transform: "scale(1.05)",
-                      }}
-                    />
-                  </Link>
-                </Box>
-                <BlogTags tags={["Engineering", "Product"]} marginTop="3" />
-                <Heading fontSize="xl" marginTop="2">
-                  <Link
-                    textDecoration="none"
-                    _hover={{ textDecoration: "none" }}
-                  >
-                    {item.blog.frontmatter?.title}
-                  </Link>
-                </Heading>
-                <Text as="p" fontSize="md" marginTop="2">
-                  {item.blog.frontmatter?.description}
-                </Text>
-                <BlogAuthor
-                  name={item.blog.frontmatter?.author!}
-                  date={item.blog.frontmatter?.date!}
+      <Divider marginTop='2' width='60px' marginX='auto' color={'black'} />
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10} marginTop='5'>
+        {blogList.map((item, index: number) => (
+          <Box
+            key={index}
+            w='100%'
+            border='1px solid'
+            borderColor='gray.200'
+            borderRadius='lg'
+            overflow='hidden'
+            p='6'
+            height='470px'
+          >
+            <Box borderRadius='lg' overflow='hidden' height='200px'>
+              <Link textDecoration='none' _hover={{ textDecoration: 'none' }}>
+                <Image
+                  transform='scale(1.0)'
+                  src={item.blog.frontmatter?.image!}
+                  alt='some text'
+                  objectFit='cover'
+                  height='100%'
+                  width='100%'
+                  transition='0.3s ease-in-out'
+                  _hover={{ transform: 'scale(1.05)' }}
                 />
-              </Box>
-            </WrapItem>
-          );
-        })}
-      </Flex>
+              </Link>
+            </Box>
+            <BlogTags tags={['Engineering', 'Product']} marginTop='3' />
+            <Heading fontSize='xl' marginTop='2' noOfLines={2}>
+              <Link href={`${item.blog.fields?.slug}`} textDecoration='none' _hover={{ textDecoration: 'none' }}>
+                {item.blog.frontmatter?.title}
+              </Link>
+            </Heading>
+            <Text as='p' fontSize='md' marginTop='2' noOfLines={3}>
+              {item.blog.frontmatter?.description}
+            </Text>
+            <BlogAuthor 
+              name={item.blog.frontmatter?.author!}
+            />
+          </Box>
+        ))}
+      </SimpleGrid>
     </Container>
   );
 };
