f7605059858e83788201f73b8968a31959da624e
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-07-12 16:09:25 +0530

    Update

--- src/views/all-blogs.tsx
+++ src/views/all-blogs.tsx
@@ -41,39 +41,36 @@ interface BlogPost {
     description: string;
     image: string;
     author: string;
+    date: string;
   };
   fields: {
     slug: string;
   };
 }
 
-const BlogTags: React.FC<IBlogTags> = ({ tags, marginTop }) => {
-  return (
-    <HStack spacing={2} marginTop={marginTop}>
-      {tags.map((tag) => (
-        <Tag size={'md'} variant='solid' colorScheme='red' key={tag}>
-          {tag}
-        </Tag>
-      ))}
-    </HStack>
-  );
-};
+const BlogTags: React.FC<IBlogTags> = ({ tags, marginTop }) => (
+  <HStack spacing={2} marginTop={marginTop}>
+    {tags.map((tag) => (
+      <Tag size={'md'} variant="solid" colorScheme="red" key={tag}>
+        {tag}
+      </Tag>
+    ))}
+  </HStack>
+);
 
-const BlogAuthor: React.FC<{ date: string; name: string }> = ({ date, name }) => {
-  return (
-    <HStack marginTop="2" spacing="2" display="flex" alignItems="center">
-      <Image
-        borderRadius="full"
-        boxSize="40px"
-        src="https://100k-faces.glitch.me/random-image"
-        alt={`Avatar of ${name}`}
-      />
-      <Text fontWeight="medium">{name}</Text>
-      <Text>—</Text>
-      <Text>{new Date(date).toLocaleDateString()}</Text>
-    </HStack>
-  );
-};
+const BlogAuthor: React.FC<{ date: string; name: string }> = ({ date, name }) => (
+  <HStack marginTop="2" spacing="2" display="flex" alignItems="center">
+    <Image
+      borderRadius="full"
+      boxSize="40px"
+      src="https://100k-faces.glitch.me/random-image"
+      alt={`Avatar of ${name}`}
+    />
+    <Text fontWeight="medium">{name}</Text>
+    <Text>—</Text>
+    <Text>{new Date(date).toLocaleDateString()}</Text>
+  </HStack>
+);
 
 const ArticleList: React.FC<AllBlogsProps> = ({ data, pageContext }) => {
   const { currentPage, numPages } = pageContext;
@@ -81,59 +78,59 @@ const ArticleList: React.FC<AllBlogsProps> = ({ data, pageContext }) => {
 
   return (
     <>
-    <Navbar />
-    <Container maxW={'7xl'} p="12">
-      <Heading as="h1">All Blogs</Heading>
-      {blogs.map(({ node }) => (
-        <Box
-          key={node.id}
-          display="flex"
-          flexDirection={{ base: 'column', md: 'row' }} // Column for mobile, Row for desktop
-          marginBottom="6" // Add margin between articles
-        >
+      <Navbar />
+      <Container maxW={'7xl'} p="12">
+        <Heading as="h1">All Blogs</Heading>
+        {blogs.map(({ node }) => (
           <Box
-            width={{ base: '100%', md: '40%' }}
-            zIndex="2"
-            marginTop="2%"
-            marginRight={{ base: '0', md: '5%' }}
-          >
-            <Link to={node.fields.slug} style={{ textDecoration: 'none' }}>
-              <Image
-                borderRadius="md"
-                src={node.frontmatter.image || 'https://via.placeholder.com/800?text=Placeholder'}
-                alt={node.frontmatter.title}
-                objectFit="cover"
-                width="98%"
-                height="auto"
-              />
-            </Link>
-          </Box>
-          <Box
-            flex="1"
+            key={node.id}
             display="flex"
-            flexDirection="column"
-            justifyContent="center"
+            flexDirection={{ base: 'column', md: 'row' }}
+            marginBottom="6"
           >
-            <BlogTags tags={['Engineering', 'Product']} marginTop="1" />
-            <Heading marginTop="1">
-              <Link to={node.fields.slug} style={{ textDecoration: 'none' }}>
-                {node.frontmatter.title}
-              </Link>
-            </Heading>
-            <Text
-              as="p"
-              marginTop="2"
-              color={useColorModeValue('gray.700', 'gray.200')}
-              fontSize="lg"
+            <Box
+              width={{ base: '100%', md: '40%' }}
+              zIndex="2"
+              marginTop="2%"
+              marginRight={{ base: '0', md: '5%' }}
             >
-              {node.frontmatter.description}
-            </Text>
-            <BlogAuthor name={node.frontmatter.author} date="2024-04-06T19:01:27Z" />
+              <Link to={node.fields.slug} style={{ textDecoration: 'none' }}>
+                <Image
+                  borderRadius="md"
+                  src={node.frontmatter.image || 'https://via.placeholder.com/800?text=Placeholder'}
+                  alt={node.frontmatter.title}
+                  objectFit="cover"
+                  width="98%"
+                  height="auto"
+                />
+              </Link>
+            </Box>
+            <Box
+              flex="1"
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+            >
+              <BlogTags tags={['Engineering', 'Product']} marginTop="1" />
+              <Heading marginTop="1">
+                <Link to={node.fields.slug} style={{ textDecoration: 'none' }}>
+                  {node.frontmatter.title}
+                </Link>
+              </Heading>
+              <Text
+                as="p"
+                marginTop="2"
+                color={useColorModeValue('gray.700', 'gray.200')}
+                fontSize="lg"
+              >
+                {node.frontmatter.description}
+              </Text>
+              <BlogAuthor name={node.frontmatter.author} date={node.frontmatter.date} />
+            </Box>
           </Box>
-        </Box>
-      ))}
-       <PaginationContainer currentPage={currentPage} numPages={numPages} />
-    </Container>
+        ))}
+        <PaginationContainer currentPage={currentPage} numPages={numPages} />
+      </Container>
     </>
   );
 };
@@ -142,6 +139,7 @@ export const query = graphql`
   query GetAllBlogPosts($skip: Int, $limit: Int) {
     allMarkdownRemark(
       filter: { frontmatter: { templateKey: { eq: "blog-page" } } }
+      sort: { fields: [frontmatter___date], order: DESC }  
       skip: $skip
       limit: $limit
     ) {
@@ -153,6 +151,7 @@ export const query = graphql`
             description
             image
             author
+            date  
           }
           fields {
             slug
